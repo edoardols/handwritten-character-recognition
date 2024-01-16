XOR and fixed backpropagation

src/XOR/lib/accuracy.py
@@ -4,7 +4,7 @@ from src.XOR.lib.sigmoid import sigMatrix
 from src.XOR.lib.activation import activation
 
 
-def output(WB0, WB1, WB2, X):
+def output(WB0, WB1, X):
 
     A0 = activation(WB0, X)
     Y0_NN = sigMatrix(A0)
@@ -14,41 +14,31 @@ def output(WB0, WB1, WB2, X):
     A1 = activation(WB1, Y0_NN)
     Y1_NN = sigMatrix(A1)
 
-    Y1_NN = np.insert(Y1_NN, Y1_NN.shape[0], np.transpose(1.0), axis=0)
-
-    A2 = activation(WB2, Y1_NN)
-    Y2_NN = sigMatrix(A2)
-    return Y2_NN
+    return Y1_NN
 
 
-def accuracy(Y, WB0, WB1, WB2, X, validation_threshold):
-    # error
-    error_label = []
-    error_patter = []
-    error_output_nn = []
+def accuracy(Y, WB0, WB1, X):
 
     a = 0
+    wrong_classified = 0
+    unclassified = 0
 
     for i in range(0, len(X)):
-        Y_NN = output(WB0, WB1, WB2, X[i])
+        Y_NN = output(WB0, WB1, X[i])
         # index of the max element in the array
         # what character the NN thinks it has recognized
         if Y_NN >= 0.8:
             y_nn = 1
-
         elif Y_NN <= 0.2:
             y_nn = 0
         else:
-            y_nn = -1
+            unclassified = unclassified + 1
 
         if y_nn == Y[i]:
             a = a + 1
         else:
-            if y_nn == -1:
-
-            error_label.append(Y[i])
-            error_patter.append(X[i])
-            error_output_nn.append(y_nn)
+            wrong_classified = wrong_classified + 1
 
     a = (a / len(Y)) * 100
-    return int(a), error_label, error_patter, error_output_nn
+
+    return int(a), int(unclassified), int(wrong_classified)

src/XOR/validation/validation.py
@@ -2,16 +2,13 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-from src.handwrittencharacter.lib.forward.accuracy import accuracy as forward_accuracy
-from src.handwrittencharacter.lib.backprop.accuracy import accuracy as backpropagation_accuracy
-from src.handwrittencharacter.lib.mapping import input_normalization_Matrix
+from src.XOR.lib.accuracy import accuracy as XOR_accuracy
 
 global current_index
 current_index = 0
 
 
 def display_validation(percentage, error_label, images, error_output_nn):
-
     fig, ax = plt.subplots()
 
     # Display the first image
@@ -56,17 +53,15 @@ def display_validation(percentage, error_label, images, error_output_nn):
     plt.show()
 
 
-def XOR_validation_single(validation_dataset_path, weight_and_biases_path, validation_threshold):
+def XOR_validation_single(validation_dataset_path, weight_and_biases_path):
     print('Validation: Start')
-    validation_dataset = pd.read_csv('../../dataset/' + validation_dataset_path + '.csv', header=None)
+    validation_dataset = pd.read_csv(validation_dataset_path + '.csv', header=None)
 
     XV_D = validation_dataset.iloc[:, 1:]
     XV = XV_D.to_numpy()
 
     YV_D = validation_dataset.iloc[:, :1]
     YV = YV_D[0].to_numpy()
-
-    XV = input_normalization_Matrix(XV)
 
     path = 'backpropagation/training/' + weight_and_biases_path + '/'
 
@@ -75,25 +70,25 @@ def XOR_validation_single(validation_dataset_path, weight_and_biases_path, valid
     W0 = w.to_numpy()
     w = pd.read_csv(path + 'W1.csv', header=None)
     W1 = w.to_numpy()
-    w = pd.read_csv(path + 'W2.csv', header=None)
-    W2 = w.to_numpy()
 
     # Biases
     b = pd.read_csv(path + 'B0.csv', header=None)
     B0 = b.to_numpy()
     b = pd.read_csv(path + 'B1.csv', header=None)
     B1 = b.to_numpy()
-    b = pd.read_csv(path + 'B2.csv', header=None)
-    B2 = b.to_numpy()
 
     # Expand matrix W with a column B
     WB0 = np.insert(W0, W0.shape[1], np.transpose(B0), axis=1)
     WB1 = np.insert(W1, W1.shape[1], np.transpose(B1), axis=1)
-    WB2 = np.insert(W2, W2.shape[1], np.transpose(B2), axis=1)
 
-    percentage, error_label, images, error_output_nn = backpropagation_accuracy(YV, WB0, WB1, WB2, XV, validation_threshold)
+    XV_hat = np.insert(XV, XV.shape[1], np.transpose(np.ones((XV.shape[0], 1), dtype=float)), axis=1)
+
+    percentage, unclassified, wrong_classified = XOR_accuracy(YV, WB0, WB1, XV_hat)
+
+    print('Accuracy: ' + str(percentage) + '%')
+    print('unclassified', unclassified)
+    print('wrong_classified', wrong_classified)
 
     print('Validation: Done')
 
-    display_validation(percentage, error_label, images, error_output_nn)
-
+    # display_validation(percentage, unclassified, wrong_classified)

src/handwrittencharacter/lib/backprop/mean_square_error.py
@@ -47,14 +47,19 @@ def empirical_risk(Y, WB0, WB1, WB2, X):
 
     for k in range(0, len(X)):
 
-
-        x = X[k]
-        x = x.reshape(1, -1)
+        if len(Y) > 1:
+            x = X[k]
+            x = x.reshape(1, -1)
+        else:
+            x = X
         Y0_NN, Y1_NN, Y2_NN, A0, A1, A2 = output(WB0, WB1, WB2, x)
 
         # first step backprop (output layer)
+        if len(Y) > 1:
+            y = one_hot_encode(Y[k])
+        else:
+            y = one_hot_encode(Y)
 
-        y = one_hot_encode(Y[k])
         de = -(y - Y2_NN) * dsigMatrix(A2)
         ek = np.dot(de, np.transpose(Y1_NN))
 
@@ -67,9 +72,11 @@ def empirical_risk(Y, WB0, WB1, WB2, X):
         E1 = E1 + ek
 
         # second step backprop (bottom hidden layer)
-        x = X[k]
+        if len(Y) > 1:
+            x = X[k]
+        else:
+            x = X
         x = x.reshape(-1, 1)
-
         ek, de = loss_function(WB1, x, A0, de)
 
         E0 = E0 + ek

src/handwrittencharacter/validation/validation.py
@@ -123,7 +123,10 @@ def backpropagation_validation_single(validation_dataset_path, weight_and_biases
     WB1 = np.insert(W1, W1.shape[1], np.transpose(B1), axis=1)
     WB2 = np.insert(W2, W2.shape[1], np.transpose(B2), axis=1)
 
-    percentage, error_label, images, error_output_nn = backpropagation_accuracy(YV, WB0, WB1, WB2, XV, validation_threshold)
+    # Expand matrix X with a column of 1s
+    XV_hat = np.insert(XV, XV.shape[1], np.transpose(np.ones((XV.shape[0], 1), dtype=float)), axis=1)
+
+    percentage, error_label, images, error_output_nn = backpropagation_accuracy(YV, WB0, WB1, WB2, XV_hat, validation_threshold)
 
     print('Validation: Done')
 