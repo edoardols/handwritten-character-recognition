moved folders and fixed forward and backpropagation training with only one folder for training

src/handwrittencharacter/backpropagation/backpropagation.py
@@ -1,9 +1,7 @@
-import pandas as pd
-import numpy as np
-
 import os
 
-from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
 
 from src.handwrittencharacter.lib.backprop.gradient import gradient_descent_algorithm
 from src.handwrittencharacter.lib.mapping import input_normalization_Matrix
@@ -11,8 +9,7 @@ from src.handwrittencharacter.lib.mapping import input_normalization_Matrix
 
 def backpropagation_training(PATH_MAIN_FILE, l, ETA, desired_epochs, learning_mode, batch_dimension):
 
-    STEP = 50
-    SUB_STEP = 10
+    STEP = 10
 
     W0 = None
     W1 = None
@@ -25,51 +22,55 @@ def backpropagation_training(PATH_MAIN_FILE, l, ETA, desired_epochs, learning_mo
     Etot = None
 
     # check if previous step exist
+    folder_found = False
 
-    folder_not_found = True
-    q = desired_epochs // STEP
-
-    global epochs, path_to_new_folder
     epochs = 0
 
-    while folder_not_found and q >= 0:
-        previous_epochs = (q + 1) * STEP
-        if learning_mode == 'mini':
-            path_to_previous_folder = (PATH_MAIN_FILE + '/backpropagation/training/' + 'B-' + learning_mode + '=' + str(batch_dimension)
-                                       + '-l=' + str(l) + '-eta=' + str(ETA) + '-epoch=' + str(previous_epochs) + '/')
-        else:
-            path_to_previous_folder = (PATH_MAIN_FILE + '/backpropagation/training/' + 'B-' + learning_mode + '-l=' + str(l)
-                        + '-eta=' + str(ETA) + '-epoch=' + str(previous_epochs) + '/')
+    path_to_main_folder = (PATH_MAIN_FILE + '/backpropagation/training/' + 'B-' + learning_mode + '-l=' + str(l) + '-eta='
+                           + str(ETA) + '/')
 
-        if os.path.exists(path_to_previous_folder):
-            folder_not_found = False
+    if learning_mode == 'mini':
+        path_to_main_folder = (PATH_MAIN_FILE + '/backpropagation/training/' + 'B-' + learning_mode + '=' + str(batch_dimension)
+                               + '-l=' + str(l) + '-eta=' + str(ETA) + '/')
 
-            for i in range(0, 5):
-                path_to_previous_epochs = (path_to_previous_folder + 'epoch=' + str(previous_epochs - SUB_STEP * i)
-                                           + '/')
-                if os.path.exists(path_to_previous_epochs):
-                    epochs = (previous_epochs - SUB_STEP * i)
+    if os.path.exists(path_to_main_folder + 'epoch=' + str(desired_epochs)):
+        return
 
-                    # Weights
-                    w = pd.read_csv(path_to_previous_epochs + 'W0.csv', header=None)
-                    W0 = w.to_numpy()
-                    w = pd.read_csv(path_to_previous_epochs + 'W1.csv', header=None)
-                    W1 = w.to_numpy()
-                    w = pd.read_csv(path_to_previous_epochs + 'W2.csv', header=None)
-                    W2 = w.to_numpy()
+    if os.path.exists(path_to_main_folder):
+        folder_found = True
+    else:
+        os.makedirs(path_to_main_folder)
 
-                    # Biases
-                    b = pd.read_csv(path_to_previous_epochs + 'B0.csv', header=None)
-                    B0 = b.to_numpy()
-                    b = pd.read_csv(path_to_previous_epochs + 'B1.csv', header=None)
-                    B1 = b.to_numpy()
-                    b = pd.read_csv(path_to_previous_epochs + 'B2.csv', header=None)
-                    B2 = b.to_numpy()
+    # - 1 so skip the desired epochs value
+    q = (desired_epochs / STEP) - 1
 
-                    # Empirical Risk
-                    e = pd.read_csv(path_to_previous_epochs + 'E.csv', header=None)
-                    Etot = e.to_numpy()
-                    break
+    while folder_found and q >= 0:
+        path_to_existing_sub_folder = (path_to_main_folder + 'epoch=' + str(q * STEP) + '/')
+
+        if os.path.exists(path_to_existing_sub_folder):
+            epochs = (q * STEP)
+
+            # Weights
+            w = pd.read_csv(path_to_existing_sub_folder + 'W0.csv', header=None)
+            W0 = w.to_numpy()
+            w = pd.read_csv(path_to_existing_sub_folder + 'W1.csv', header=None)
+            W1 = w.to_numpy()
+            w = pd.read_csv(path_to_existing_sub_folder + 'W2.csv', header=None)
+            W2 = w.to_numpy()
+
+            # Biases
+            b = pd.read_csv(path_to_existing_sub_folder + 'B0.csv', header=None)
+            B0 = b.to_numpy()
+            b = pd.read_csv(path_to_existing_sub_folder + 'B1.csv', header=None)
+            B1 = b.to_numpy()
+            b = pd.read_csv(path_to_existing_sub_folder + 'B2.csv', header=None)
+            B2 = b.to_numpy()
+
+            # Empirical Risk
+            e = pd.read_csv(path_to_existing_sub_folder + 'E.csv', header=None)
+            Etot = e.to_numpy()
+            break
+
         q = q - 1
 
     print('Loading dataset: Start')
@@ -129,9 +130,9 @@ def backpropagation_training(PATH_MAIN_FILE, l, ETA, desired_epochs, learning_mo
         Etot = []
 
     while epochs < desired_epochs:
-        E = np.zeros(min(desired_epochs, SUB_STEP), dtype=float)
+        E = np.zeros(min(desired_epochs, STEP), dtype=float)
 
-        for e in range(0, min(desired_epochs, SUB_STEP)):
+        for e in range(0, min(desired_epochs, STEP)):
             WB0, WB1, WB2, E_epoch = gradient_descent_algorithm(D, WB0, WB1, WB2, ETA, epochs + e, learning_mode, batch_dimension)
             E[e] = E_epoch
 
@@ -139,69 +140,38 @@ def backpropagation_training(PATH_MAIN_FILE, l, ETA, desired_epochs, learning_mo
 
         print('Saving: Start')
 
-        q = epochs // STEP
-        r = epochs % STEP
-        folder_epochs = q * STEP
-        if r >= 0 or q == 0:
-            folder_epochs = (q + 1) * STEP
+        epochs = epochs + STEP
 
-        if learning_mode == 'mini':
-            path_to_new_folder = (PATH_MAIN_FILE +
-                        '/backpropagation/training/' + 'B-' + learning_mode + '=' + str(batch_dimension) + '-l=' + str(l) + '-eta=' + str(ETA) +
-                        '-epoch=' + str(folder_epochs) + '/')
-        else:
-            path_to_new_folder = (PATH_MAIN_FILE + '/backpropagation/training/' + 'B-' + learning_mode + '-l=' + str(l) + '-eta='
-                                  + str(ETA) + '-epoch=' + str(folder_epochs) + '/')
+        path_to_new_sub_folder = (path_to_main_folder + 'epoch=' + str(epochs) + '/')
 
-        sub_folder_path = (path_to_new_folder + 'epoch=' + str(epochs + SUB_STEP) + '/')
-
-        # Check if the folder already exists
-        if not os.path.exists(sub_folder_path):
+        if not os.path.exists(path_to_new_sub_folder):
             # Create the folder if it doesn't exist
-            os.makedirs(sub_folder_path)
+            os.makedirs(path_to_new_sub_folder)
 
         # W0 and B0
         weight = pd.DataFrame(WB0[:, :WB0.shape[1] - 1])
-        weight.to_csv(sub_folder_path + 'W0.csv', encoding='utf-8', header=False, index=False)
+        weight.to_csv(path_to_new_sub_folder + 'W0.csv', encoding='utf-8', header=False, index=False)
 
         bias = pd.DataFrame(WB0[:, WB0.shape[1] - 1:])
-        bias.to_csv(sub_folder_path + 'B0.csv', encoding='utf-8', header=False, index=False)
+        bias.to_csv(path_to_new_sub_folder + 'B0.csv', encoding='utf-8', header=False, index=False)
 
         # W1 and B1
         weight = pd.DataFrame(WB1[:, :WB1.shape[1] - 1])
-        weight.to_csv(sub_folder_path + 'W1.csv', encoding='utf-8', header=False, index=False)
+        weight.to_csv(path_to_new_sub_folder + 'W1.csv', encoding='utf-8', header=False, index=False)
 
         bias = pd.DataFrame(WB1[:, WB1.shape[1] - 1:])
-        bias.to_csv(sub_folder_path + 'B1.csv', encoding='utf-8', header=False, index=False)
+        bias.to_csv(path_to_new_sub_folder + 'B1.csv', encoding='utf-8', header=False, index=False)
 
         # W2 and B2
         weight = pd.DataFrame(WB2[:, :WB2.shape[1] - 1])
-        weight.to_csv(sub_folder_path + 'W2.csv', encoding='utf-8', header=False, index=False)
+        weight.to_csv(path_to_new_sub_folder + 'W2.csv', encoding='utf-8', header=False, index=False)
 
         bias = pd.DataFrame(WB2[:, WB2.shape[1] - 1:])
-        bias.to_csv(sub_folder_path + 'B2.csv', encoding='utf-8', header=False, index=False)
+        bias.to_csv(path_to_new_sub_folder + 'B2.csv', encoding='utf-8', header=False, index=False)
 
         empirical = pd.DataFrame(Etot)
-        empirical.to_csv(sub_folder_path + 'E.csv', encoding='utf-8', header=False, index=False)
+        empirical.to_csv(path_to_new_sub_folder + 'E.csv', encoding='utf-8', header=False, index=False)
 
         print('Saving: Done')
-
-        epochs = epochs + SUB_STEP
-
-        if epochs // STEP:
-            # plot
-            x = np.arange(0, epochs, 1)
-            y = Etot
-            plt.plot(x, y, color='cyan')
-
-            plt.xlabel('Epochs')
-            plt.ylabel('Empirical risk')
-            annotation_string = (r'$\eta$ = ' + str(ETA) + '\n'
-                                 + '#Patterns = ' + str(l) + '\n'
-                                 + 'Learning mode = ' + learning_mode + '\n')
-
-            plt.annotate(annotation_string, xy=(0.88, 0.72), xycoords='figure fraction', horizontalalignment='right')
-
-            plt.savefig(path_to_new_folder + 'E')
 
     print('Training: Done')

src/handwrittencharacter/validation/backprop.py
@@ -1,31 +1,18 @@
-import os
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
 from src.handwrittencharacter.lib.forward.accuracy import accuracy as forward_accuracy
-from src.handwrittencharacter.lib.backprop.accuracy import accuracy as backprop_accuracy
+from src.handwrittencharacter.lib.backprop.accuracy import accuracy as backpropagation_accuracy
 from src.handwrittencharacter.lib.mapping import input_normalization_Matrix
 
 global current_index
 current_index = 0
 
 
-def forward_validation_graph(PATH_MAIN_FILE, validation_dataset_name, weight_and_biases_path, validation_threshold):
+def backpropagation_validation_single(PATH_MAIN_FILE, validation_dataset_path, weight_and_biases_path, validation_threshold):
     print('Validation: Start')
-
-    folders = weight_and_biases_path.split('/')
-
-    parameters = folders[0].split('-')
-
-    pattern = parameters[2].split('=')[1]
-    eta = parameters[3].split('=')[1]
-    learning_mode = parameters[1]
-    epochs = int(folders[1].split('=')[1])
-
-    validation_dataset_path = PATH_MAIN_FILE + '/../../dataset/' + validation_dataset_name + '.csv'
-    validation_dataset = pd.read_csv(validation_dataset_path, header=None)
+    validation_dataset = pd.read_csv(PATH_MAIN_FILE + '/../../dataset/' + validation_dataset_path + '.csv', header=None)
 
     XV_D = validation_dataset.iloc[:, 1:]
     XV = XV_D.to_numpy()
@@ -35,72 +22,37 @@ def forward_validation_graph(PATH_MAIN_FILE, validation_dataset_name, weight_and
 
     XV = input_normalization_Matrix(XV)
 
-    # STEP = 500
-    # SUB_STEP = 100
-    # for i in range(SUB_STEP, epochs, SUB_STEP):
-    #     q = epochs // STEP
-    #     folder = 'W-F-' + str(learning_mode) + '-l=' + str(pattern) + '-epoch=' + str((q + 1) * 500) + '-eta=' + str(eta)
-    #     sub_folder = 'W-F-' + str(learning_mode) + '-l=' + str(pattern) + '-epoch=' + str(i) + '-eta=' + str(eta)
-    #     weight_and_biases_path = folder + '/' + sub_folder
+    path = PATH_MAIN_FILE + '/backpropagation/training/' + weight_and_biases_path + '/'
 
-    accuracy = np.zeros(epochs // 100)
-    for i in range(500, epochs + 500, 500):
-        parent_folder = 'F-' + str(learning_mode) + '-l=' + str(pattern) + '-eta=' + str(eta) + '-epoch=' + str(i)
-        for j in range(i - 400, i + 100, 100):
-            if j > epochs:
-                break
-            child_folder = 'epoch=' + str(j)
-            weight_and_biases_path = parent_folder + '/' + child_folder
+    # Weights
+    w = pd.read_csv(path + 'W0.csv', header=None)
+    W0 = w.to_numpy()
+    w = pd.read_csv(path + 'W1.csv', header=None)
+    W1 = w.to_numpy()
+    w = pd.read_csv(path + 'W2.csv', header=None)
+    W2 = w.to_numpy()
 
-            w = pd.read_csv(PATH_MAIN_FILE + '/forward/training/' + weight_and_biases_path + '/W.csv', header=None)
-            W = w.to_numpy()
+    # Biases
+    b = pd.read_csv(path + 'B0.csv', header=None)
+    B0 = b.to_numpy()
+    b = pd.read_csv(path + 'B1.csv', header=None)
+    B1 = b.to_numpy()
+    b = pd.read_csv(path + 'B2.csv', header=None)
+    B2 = b.to_numpy()
 
-            b = pd.read_csv(PATH_MAIN_FILE + '/forward/training/' + weight_and_biases_path + '/B.csv', header=None)
-            B = b.to_numpy()
+    # Expand matrix W with a column B
+    WB0 = np.insert(W0, W0.shape[1], np.transpose(B0), axis=1)
+    WB1 = np.insert(W1, W1.shape[1], np.transpose(B1), axis=1)
+    WB2 = np.insert(W2, W2.shape[1], np.transpose(B2), axis=1)
 
-            # Bias learnable
-            # Expand matrix W with a column B
-            WB = np.insert(W, W.shape[1], np.transpose(B), axis=1)
+    # Expand matrix X with a column of 1s
+    XV_hat = np.insert(XV, XV.shape[1], np.transpose(np.ones((XV.shape[0], 1), dtype=float)), axis=1)
 
-            # Expand matrix X with a column of 1s
-            XV_hat = np.insert(XV, XV.shape[1], np.transpose(np.ones((XV.shape[0], 1), dtype=float)), axis=1)
+    percentage, error_label, images, error_output_nn = backpropagation_accuracy(YV, WB0, WB1, WB2, XV_hat, validation_threshold)
 
-            percentage, error_label, images, error_output_nn = forward_accuracy(YV, WB, XV_hat, validation_threshold)
+    print('Validation: Done')
 
-            accuracy[(j // 100) - 1] = percentage
-
-            # print('Validation: Done')
-
-            # display_validation(percentage, error_label, images, error_output_nn)
-
-    # plot
-    x = np.arange(100, epochs + 100, 100)
-    y = accuracy
-    plt.plot(x, y, color='cyan')
-
-    plt.xlabel('Epochs')
-    plt.ylabel('Empirical risk')
-    plt.title('Threshold: ' + str(int(validation_threshold*100)) + '%')
-    annotation_string = (r'$\eta$ = ' + str(eta) + '\n'
-                         + '#Patterns = ' + str(pattern) + '\n'
-                         + 'Learning mode = ' + learning_mode + '\n')
-
-    plt.annotate(annotation_string, xy=(0.88, 0.72), xycoords='figure fraction', horizontalalignment='right')
-
-    parent_figure_folder = (PATH_MAIN_FILE + '/validation/figure-forward/' + str(validation_dataset_name) + '/threshold='
-                            + str(validation_threshold) + '/')
-
-    parent_figure_folder = os.path.join(os.getcwd(), parent_figure_folder)
-
-    child_figure_folder = (parent_figure_folder + '/F-' + str(learning_mode) + '-l=' + str(pattern) + '-eta=' + str(eta)
-                          + '-epoch=' + str(epochs))
-
-    if not os.path.exists(parent_figure_folder):
-        # Create the folder if it doesn't exist
-        os.makedirs(parent_figure_folder)
-    plt.savefig(child_figure_folder + '.png')
-
-    plt.show()
+    display_validation(percentage, error_label, images, error_output_nn)
 
 
 def backpropagation_validation_graph(PATH_MAIN_FILE, validation_dataset_path, learning_mode, pattern, epochs, eta, validation_threshold):