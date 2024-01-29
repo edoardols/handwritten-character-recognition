70f4a259f620dfcbc521716e7f605ad680854d56
gsalvatore99 <beppe0032@gmail.com>
2024-01-29 11:15:09 +0100

    backprop mini 128 eta 0.0001

--- src/handwrittencharacter/main.py
+++ src/handwrittencharacter/main.py
@@ -1,40 +1,85 @@
+# region IMPORTS
 import os
 
 from src.handwrittencharacter.forward.forward import forward_training
 from src.handwrittencharacter.backpropagation.backpropagation import backpropagation_training
 
-# from src.handwrittencharacter.validation.validation import forward_validation_single
-# from src.handwrittencharacter.validation.validation_plot import forward_validation_graph
-#
-# from src.handwrittencharacter.validation.validation import backpropagation_validation_single
-# from src.handwrittencharacter.validation.validation_plot import backpropagation_validation_graph
+from src.handwrittencharacter.validation.forward import forward_validation_single
+from src.handwrittencharacter.validation.forward import forward_validation_graph
+
+from src.handwrittencharacter.validation.backprop import backpropagation_validation_single
+from src.handwrittencharacter.validation.backprop import backpropagation_validation_graph
+# endregion
 
 PATH_MAIN_FILE = os.path.dirname(__file__)
 
+# region SETTINGS
+# Type ( B / F )
+Type = 'B'
+
+# Program ( train / validation / graph / all )
+program = 'graph'
+
 # Parameters
-l = 60  # Number of examples
-ETA = 0.01  # learning rate
+l = 60000  # Number of examples
+ETA = 0.0001  # learning rate
 epochs = 500  # epochs
+STEP = 10  # for validation graph
 
-# Learning method
-# learning_mode = 'batch'
+# Learning method ( batch / mini / online )
 learning_mode = 'mini'
-batch_dimension = 512
-# learning_mode = 'online'
+batch_dimension = 128
 
-# Training
-forward_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
-# backpropagation_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
-
-# Validation
+# Validation ( mnist_test / blob/mnist_test-bl-p-0.2 / ... )
 validation_dataset = 'mnist_test'
 validation_threshold = 0.0
+# endregion
 
-weight_and_biases_path = 'F-mini=128-l=60000-eta=0.01-epoch=1000/epoch=900'
-weight_and_biases_path = 'F-batch-l=60000-eta=0.00001-epoch=500/epoch=100'
-# forward_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
-# forward_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+# region Program
+# NN that u want to validate
+if learning_mode == 'mini':
+    weight_and_biases = Type + '-' + learning_mode + '=' + str(batch_dimension) + '-l=' + str(l) + '-eta=' + str(ETA)
+else:
+    weight_and_biases = Type + '-' + learning_mode + '-l=' + str(l) + '-eta=' + str(ETA)
 
-weight_and_biases_path = 'B-mini=512-l=60000-eta=0.01-epoch=500/epoch=500'
-# backpropagation_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
-# backpropagation_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+weight_and_biases_path = weight_and_biases + '/epoch=' + str(epochs)
+
+print('Object of the program: ' + weight_and_biases_path)
+
+if Type == 'F':
+    if program == 'train':
+        forward_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
+    elif program == 'validation':
+        forward_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+    elif program == 'graph':
+        forward_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+    elif program == 'all':
+        forward_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
+        forward_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+        forward_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, validation_threshold)
+    else:
+        print('Wrong settings: try use one of the following --> train / validation / both')
+        exit()
+
+elif Type == 'B':
+    if program == 'train':
+        backpropagation_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
+    elif program == 'validation':
+        backpropagation_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path,
+                                          validation_threshold)
+    elif program == 'graph':
+        backpropagation_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, epochs, STEP,
+                                         validation_threshold)
+    elif program == 'all':
+        backpropagation_training(PATH_MAIN_FILE, l, ETA, epochs, learning_mode, batch_dimension)
+        backpropagation_validation_single(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path,
+                                          validation_threshold)
+        backpropagation_validation_graph(PATH_MAIN_FILE, validation_dataset, weight_and_biases_path, epochs, STEP,
+                                         validation_threshold)
+    else:
+        print('Wrong settings: try use one of the following --> train / validation / both')
+        exit()
+else:
+    print('Wrong settings: try use one of the following --> F / B')
+    exit()
+# endregion
